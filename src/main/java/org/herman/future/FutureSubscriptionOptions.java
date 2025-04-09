3c7c095893fa39a4c50b72638ec7cbadc0647c4a
Herman <380303318@qq.com>
2025-04-09 14:28:29 +0800

    initial

--- src/main/java/org/herman/future/FutureSubscriptionOptions.java
+++ src/main/java/org/herman/future/FutureSubscriptionOptions.java
@@ -11,7 +11,7 @@ public abstract class FutureSubscriptionOptions {
 
     private String uri;
     private boolean isAutoReconnect = true;
-    private int receiveLimitMs = 300_000;
+    private int receiveLimitMs = 100_000;
     private int connectionDelayOnFailure = 1;
 
     public FutureSubscriptionOptions(String uri) {
