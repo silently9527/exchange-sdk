3c67084a9c09ae20eb543e52a3b5884a10a67b83
Herman <380303318@qq.com>
2025-08-01 20:47:41 +0800

    initial

--- src/main/java/org/herman/future/impl/WebSocketConnectionPool.java
+++ src/main/java/org/herman/future/impl/WebSocketConnectionPool.java
@@ -1,6 +1,7 @@
 package org.herman.future.impl;
 
 import org.herman.future.FutureSubscriptionOptions;
+import org.herman.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,9 +12,11 @@ public class WebSocketConnectionPool {
     private static final Logger log = LoggerFactory.getLogger(WebSocketConnectionPool.class);
 
     private final WebSocketWatchDog watchDog;
+    private final FutureSubscriptionOptions options;
     private final List<WebSocketConnection> connections = new ArrayList<>();
 
     public WebSocketConnectionPool(FutureSubscriptionOptions options) {
+        this.options = options;
         this.watchDog = new WebSocketWatchDog(options);
         if (options.getConnectionCount() <= 0) {
             throw new IllegalArgumentException("FutureSubscriptionOptions.connectionCount must gt 0");
@@ -27,6 +30,13 @@ public class WebSocketConnectionPool {
     }
 
     public synchronized WebSocketConnection get() {
+        if (connections.size() < options.getConnectionCount()) {
+            WebSocketConnection connection = new WebSocketConnection(options, watchDog);
+            connection.connect();
+            Utils.sleepSeconds(1);
+            connections.add(connection);
+        }
+
         int index = (int) (Math.random() * connections.size());
         return connections.get(index);
     }
